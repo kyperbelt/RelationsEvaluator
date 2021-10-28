1e9ce0fe0d005ce24254affd42668c9c5c80f32f
kyperbelt <jonathancamarenadev@gmail.com>
2021-10-28 00:45:46 -0700

    added utility functions parseSet and parseRelations in order to iterate faster and to later implement a command line interface.

--- relations_evaluator.cpp
+++ relations_evaluator.cpp
@@ -1,5 +1,6 @@
 #include "relations_evaluator.h"
 #include <iostream>
+#include <locale>
 
 
 using namespace std;
@@ -22,4 +23,115 @@ void runTest1(){
 
 void runTest2(){
     cout << "tests2" << endl;
+
+
+    string s = "a,b,c,d,e,f,g";
+
+    set<char> s1 = parseSet(s);
+
+    cout << "set:" << endl;
+    for(char item: s1){
+        cout << item << endl;
+    }
+
+    vector<Tuple> relations = parseRelations("(a,b) (c,d) (b,a)",s1);
+
+    cout << "relations: " << endl;
+    for(Tuple t: relations){
+
+        cout << tupleToString(t) << endl;
+
+    }
+
+}
+
+
+std::string tupleToString(Tuple t){
+    string result;
+
+    result.push_back('(');
+    result.push_back(t.left);
+    result.push_back(',');
+    result.push_back(' ');
+    result.push_back(t.right);
+    result.push_back(')');
+
+    return result;
+}
+
+set<char> parseSet(string input){
+    set<char> result;
+
+    for(int i=0; i < input.size(); i++){
+        char c = input[i];
+        if (c == ' ' || c == ',') continue; // this is a space or a comma so we skip it
+        result.insert(c);
+    }
+
+    return result;
+}
+
+
+std::vector<Tuple> parseRelations(std::string input,std::set<char> &s){
+    vector<Tuple> result;
+
+    const int BASE = 0;
+    const int ERROR = 1;
+    const int TUPLE = 2;
+    const int CLOSE = 3;
+
+    int currentState = BASE;
+
+    char left = '\0';
+
+    for(int i=0; i< input.size(); i++){
+        char c = input[i];
+        switch(currentState){
+            case BASE:{
+               // in the base case we ignore all spaces and commas, we are looking for '(' to bring a tuple
+                if (c == '('){
+                    currentState = TUPLE;
+                }else if (c == ' ' || c == ','){
+                    continue;
+                }else{
+                    // we have encountered an invalid vaiable
+                    //TODO: throw an error or notify user of mistake
+                    cerr << "invalid chacacter on line:" << input << " at index:" << i
+                         << " was expecting a tuple opening char '(' got '" << c << "' instead." << endl;
+                }
+            }
+            break;
+            case TUPLE: {
+                if (isalnum(c)){
+                    if (s.count(c) == 0){
+                        cerr << "Invalid element <" << c << "> @index:" << i << " not found in set provided." << endl;
+                        currentState = ERROR;
+                    }else{
+                        if (left == '\0'){
+                            left = c;
+                        }else{
+                            result.push_back(Tuple{left,c});
+                            left = '\0';
+                            currentState = CLOSE;
+                        }
+                    }
+                }else if(c == ')'){
+                    currentState = BASE;
+                }else{
+                    continue;
+                }
+            }
+                break;
+            case ERROR: {
+                cerr << "discarding " << c << " at index:" << i << endl;
+            }
+            case CLOSE: {
+                if (c == ')') currentState = BASE;
+            }break;
+            default: break; // reached an invalid state
+        }
+
+    }
+
+    return result;
 }
--- relations_evaluator.h
+++ relations_evaluator.h
@@ -3,6 +3,7 @@
 
 #include <set>
 #include <vector>
+#include <string>
 
 struct Tuple{
     char left = '\0';
@@ -19,5 +20,30 @@ bool isComparable(std::vector<Tuple> relations,std::set<char> s);
 
 void runTest1();
 void runTest2();
+
+
+
+/**
+ * parser the input string into a vector of valid tuples over the set s
+ *
+ * @example input: "(a,b) (b,a)"
+ *
+ * @return vector<Tuple> relations
+ **/
+std::vector<Tuple> parseRelations(std::string input,std::set<char> &s);
+
+
+/**
+ * parse the input string into a valid set
+ *
+ * @example input: "a b c d"
+ *
+ * 
+ * @return set::<>
+ **/
+std::set<char>     parseSet(std::string input);
+
+
+std::string tupleToString(Tuple t);
 
 #endif
