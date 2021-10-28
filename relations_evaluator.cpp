31a70cbcf4c3c159d29846166c5b1f429cac26a0
jfwang983 <jfwang983@gmail.com>
2021-10-28 00:22:07 -0700

    isReflexive finished

--- relations_evaluator.cpp
+++ relations_evaluator.cpp
@@ -1,10 +1,10 @@
 #include "relations_evaluator.h"
 #include <iostream>
-
-
+#include <map>
+#include <algorithm>
 using namespace std;
 
-
+bool isReflexive(vector<Tuple> relations, set<char> s);
 
 int main(){
     runTest1();
@@ -15,11 +15,38 @@ int main(){
     return 0;
 }
 
+bool isReflexive(vector<Tuple> relations, vector<char> s) {
+    for(int i = 0; i < s.size(); i++) {
+        bool validCase = false;
+        for(int j = 0; j < relations.size(); j++) {
+            if(relations[j].left == s[i] && relations[j].left == relations[j].right) {
+                validCase = true;
+            }
+        }
+        if(!validCase) {
+            return false;
+        }
+    }
+     return true;
+}
+
 void runTest1(){
+    Tuple t1;
+    Tuple t2;
+    t1.left = 'a';
+    t1.right = 'a';
+    t2.left = 'b';
+    t2.right = 'b';
+    vector<Tuple> relations;
+    relations.push_back(t1);
+    relations.push_back(t2);
 
-    cout << "tests1" << endl;
+    vector<char> set;
+    set.push_back('a');
+    set.push_back('b');
+
+    // Reflexive
+    cout << isReflexive(relations, set) << endl;
 }
 
-void runTest2(){
-    cout << "tests2" << endl;
-}
+void runTest2(){}
